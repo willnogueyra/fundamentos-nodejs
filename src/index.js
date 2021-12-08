d7f53f9f2d359e6126efa815cff06dc14a5878cd
William Nogueira <will.ribeiro@live.com>
2021-12-08 18:17:11 -0300

    Nodemon na aplicação

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: "Hello World!"})
+  return response.json({message: "Hello World! Fundamentos Nodejs"})
 })
 
 app.listen(3333) // localhost:3333
